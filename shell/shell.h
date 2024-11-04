db53635ac0a60dbd2538eef94b02e93ba66b2ec2
Hamzaoui-Louai <l_hamzaoui@estin.dz>
2024-11-04 22:21:16 +0100

    TOOLS BAR DRAWN INTO PLACE added the function that draws the tools bar which lists the diffrent tools that the game engine offers currently it's still set with default parameters until I make the layout organizer

--- shell/shell.h
+++ shell/shell.h
@@ -31,11 +31,39 @@ namespace Shell
             gotoxy(StartX,StartY);
             std::cout << "\033[32m welcome to V3rfy \n  if you're new here type 'help' to get started\n >>> ";
         }
+        void drawToolsBar(short StartX = 0,short StartY = 0,short EndX = 0, short EndY = 0)
+        {
+            for(short i = StartY;i<=EndY;i++)
+            {
+                gotoxy(StartX,i);
+                std::cout << '|';
+            }
+            gotoxy(StartX+1,StartY);
+            std::cout << "\033[1mTools :";
+        }
     public:
+        void getShellCorners(short &Left,short &Top,short &Right,short &Bottom)
+        {
+            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+            GetConsoleScreenBufferInfo(hConsole, &csbi);
+            Left = csbi.srWindow.Left;
+            Top = csbi.srWindow.Top;
+            Right = csbi.srWindow.Right;
+            Bottom = csbi.srWindow.Bottom;
+        }
+        void getShellDimensions(short &Width,short &Height)
+        {
+            short Left,Top,Right,Bottom;
+            getShellCorners(Left,Top,Right,Bottom);
+            Width = Right - Left + 1;
+            Height = Bottom - Top + 1;
+        }
         void start()
         {
             clearScreen();
-            drawMainShell(0,5);
+            drawToolsBar(100,0,120,30);
+            drawMainShell(0,1);         
         }
     };
 }
